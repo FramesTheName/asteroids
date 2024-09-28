9eb05a13f796781e91a7c5d1d7564357c199917b
FramesTheName <frame.mlf@gmail.com>
2024-09-28 12:42:44 -0600

    added shots to the game

--- main.py
+++ main.py
@@ -10,14 +10,20 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
+    
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
     Asteroid.containers = (updatable, drawable, asteroids)
     AsteroidField.containers = (updatable)
+    Shot.containers = (updatable, drawable)
+    
     player = Player(x,y)
     asteroid_field = AsteroidField()
 
