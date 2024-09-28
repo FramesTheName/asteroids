b96ca5eda5b69f2834ff096762b35ed62f0e2da1
FramesTheName <frame.mlf@gmail.com>
2024-09-28 13:23:09 -0600

    Added asteroid splitting

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,6 @@
+import random
 from circleshape import *
+from constants import *
 
 class Asteroid(CircleShape):
     
@@ -10,3 +12,16 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += (self.velocity * dt)
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+           new_rad = self.radius - ASTEROID_MIN_RADIUS
+           random_angle = random.uniform(20, 50)
+           top_asteroid = Asteroid(self.position.x, self.position.y, new_rad)
+           top_asteroid.velocity = (self.velocity.rotate(random_angle) * 1.2)
+           bottom_asteroid = Asteroid(self.position.x, self.position.y, new_rad)
+           bottom_asteroid.velocity = (self.velocity.rotate(-random_angle) * 1.2)
+           
